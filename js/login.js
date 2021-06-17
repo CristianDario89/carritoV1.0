1c4812370784573a0296363e0ef8d7c33c4abd4e
Cristiano10 <ccristiandarioromero@gmail.com>
2021-06-17 20:57:12 -0300

    init

---
+++ js/login.js
@@ -0,0 +1,54 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+form.addEventListener("submit", function(e) {
+e.preventDefault();
+let users = Array(
+    {
+        usuario:username.value,
+        contraseña:password.value
+    }
+);
+console.log(users);
+
+localStorage.setItem("user", JSON.stringify(users));
+
+validate();
+
+location.href = "index.html"
+});
+
+function validate(){
+const username2 = document.getElementById("username").value;
+const password2 = document.getElementById("password").value;
+ 
+if(username2 == 'berrako' && password2 == '123456'){
+         alert("Login exitoso pa")
+        return false;
+    }else{
+        alert("Usuario o contraseña incorrecta");
+        location.href = "login.html";
+    }
+    validarEspacios();
+  
+}
+
+function validarEspacios(){
+        
+    var espacios = false;
+    var cont = 0;
+
+    while (!espacios && (cont < p1.length)) {
+    if (p1.charAt(cont) == " ")
+        espacios = true;
+    cont++;
+    }
+    
+    if (espacios) {
+    alert ("La contraseña no puede contener espacios en blanco");
+    return false;
+    }
+
+ 
+}
---
+++ js/nav.js
@@ -0,0 +1,15 @@
+const usernav = document.getElementById("usernav");
+const close = document.getElementById("close");
+
+let username = JSON.parse(localStorage.getItem('user'));
+
+if(username != null){
+    usernav.innerHTML = '<a href="#" id="close" class="nav-item nav-link active ">'+"hola "+username[0].usuario+'</a>';
+}else{
+    usernav.innerHTML = '<a href="#" id="close" class="nav-item nav-link active ">Iniciar sesion</a>';
+}
+
+close.addEventListener('click', function(){
+    localStorage.clear('user');
+    location.href = 'login.html'
+});
